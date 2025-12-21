b04128d267f31debab7a4f13035a7fb4140aa8b2
Bridge Byte Tech <bridgebytetech@gmail.com>
2025-12-21 18:00:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Production-এও যেকোনো ডোমেইন থেকে ইমেজ লোড করার জন্য এই কনফিগারেশন
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "http",
+        hostname: "**",
       },
       {
-        protocol: 'http',
-        hostname: '**',
+        protocol: "https",
+        hostname: "**",
       },
     ],
   },
+
+  // API রিকোয়েস্ট ব্যাকএন্ডে পাঠানোর জন্য rewrites কনফিগারেশন
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
+        source: "/api/:path*",
         destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
