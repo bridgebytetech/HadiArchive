462938e9babe79e8f64d58fe31195dd74c9a7e83
Bridge Byte Tech <bridgebytetech@gmail.com>
2025-12-21 14:00:41 +0100

    Update publicWritingService.ts

--- services/publicWritingService.ts
+++ services/publicWritingService.ts
@@ -4,4 +4,7 @@ import { PagedResponse, Writing } from "@/types";
 export const writingPublicService = {
   getAll: (page = 0): Promise<PagedResponse<Writing>> =>
     apiRequest(api.get(`/writings?page=${page}`)),
+
+  getById: (id: string): Promise<Writing> =>
+    apiRequest(api.get(`/writings/${id}`)),
 };
