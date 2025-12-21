a0dcbd0c42984461b2d8c11d73673c2e74c2a7e5
Bridge Byte Tech <bridgebytetech@gmail.com>
2025-12-21 13:54:46 +0100

    Create publicPoemService.ts

---
+++ services/publicPoemService.ts
@@ -0,0 +1,7 @@
+import api, { apiRequest } from "@/lib/api";
+import { PagedResponse, Poem } from "@/types";
+
+export const poemPublicService = {
+  getAll: (page = 0): Promise<PagedResponse<Poem>> =>
+    apiRequest(api.get(`/poems?page=${page}`)),
+};
