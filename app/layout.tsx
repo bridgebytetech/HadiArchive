89ea1d98ddbb0c0f4e6a2ad409818ad0348475d5
Bridge Byte Tech <bridgebytetech@gmail.com>
2025-12-21 14:57:43 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toast";
 import Providers from "./providers";
@@ -72,6 +73,20 @@ export default function RootLayout({
           {children}
           <Toaster />
         </Providers>
+
+        {/* ✅ Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NSCWXWSDXQ"
+          strategy="afterInteractive"
+        />
+        <Script strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NSCWXWSDXQ');
+          `}
+        </Script>
       </body>
     </html>
   );
