ea9d0e5ab1059d92e64b7603211eafcb48972b1c
Bridge Byte Tech <bridgebytetech@gmail.com>
2025-12-21 14:01:46 +0100

    Update publicPoemService.ts

--- services/publicPoemService.ts
+++ services/publicPoemService.ts
@@ -4,4 +4,7 @@ import { PagedResponse, Poem } from "@/types";
 export const poemPublicService = {
   getAll: (page = 0): Promise<PagedResponse<Poem>> =>
     apiRequest(api.get(`/poems?page=${page}`)),
+
+  getById: (id: string): Promise<Poem> =>
+    apiRequest(api.get(`/poems/${id}`)),
 };
