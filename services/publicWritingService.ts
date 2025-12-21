5cba3e0fab2074ab66bb5515c2b84d642f5a37b4
Bridge Byte Tech <bridgebytetech@gmail.com>
2025-12-21 13:52:23 +0100

    Create publicWritingService.ts

---
+++ services/publicWritingService.ts
@@ -0,0 +1,7 @@
+import api, { apiRequest } from "@/lib/api";
+import { PagedResponse, Writing } from "@/types";
+
+export const writingPublicService = {
+  getAll: (page = 0): Promise<PagedResponse<Writing>> =>
+    apiRequest(api.get(`/writings?page=${page}`)),
+};
